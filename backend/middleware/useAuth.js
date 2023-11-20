87aac81b6013c8e5b72a29a23f3701640b4329bf
priyanshuprasad22 <priyanshuprasad925@gmail.com>
2023-11-20 11:04:06 +0530

    Next Commit

--- backend/middleware/useAuth.js
+++ backend/middleware/useAuth.js
@@ -24,10 +24,6 @@ const requireAuth =async ( req,res,next) =>{
         console.log(error)
         res.status(401).json({error: 'Request is not authorized'})
     }
-
-
-
-
 }
 
 module.exports = requireAuth
